50d417a94279066f0d91d6b55cc4cc0df1328b59
Max Troeger <max@maxtroeger.xyz>
2022-05-04 13:26:10 -0400

    Whoops forgot to push

--- config.h
+++ config.h
@@ -4,9 +4,9 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"terminus:size=10", "joypixels:size=10"
+	"liberationmono:size=10", "JoyPixels:size=10"
 };
-static const unsigned int bgalpha = 0xda;
+static const unsigned int bgalpha = 0xcc;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
