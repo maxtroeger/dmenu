e2e76d22b68ef9f123a5a69eb8a137ac90d0380d
Max Troeger <max@maxtroeger.xyz>
2022-05-09 17:04:05 -0400

    new colors

--- config.h
+++ config.h
@@ -4,16 +4,16 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"liberationmono:size=10", "JoyPixels:size=10"
+	"monospace:size=10", "JoyPixels:size=10"
 };
 static const unsigned int bgalpha = 0xcc;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#828282", "#000000" },
-	[SchemeSel] = { "#fefefe", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#222222", "#444444" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
 	/*		fgalpha		bgalphga	*/
